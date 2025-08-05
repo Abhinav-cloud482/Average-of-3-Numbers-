63ba836288c1e71d7e77afc2151858eb351b85f0
Abhinav-cloud482 <a74835655@gmail.com>
2025-08-06 01:19:28 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter 1st number:  "))
+num2 = float(input("Enter 2nd number:  "))
+num3 = float(input("Enter 3rd number:  "))
+
+average = (num1 + num2 + num3) / 3
+
+print(average)
